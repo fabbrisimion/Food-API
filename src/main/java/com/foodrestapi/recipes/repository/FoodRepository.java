4c338cb70340b027e0d29598d81538bada5e1559
Simion Fabbri <fabbrisimion9@gmail.com>
2022-08-09 22:04:23 +0300

    Food and Recipe repositories

---
+++ src/main/java/com/foodrestapi/recipes/repository/FoodRepository.java
@@ -0,0 +1,7 @@
+package com.foodrestapi.recipes.repository;
+
+import com.foodrestapi.recipes.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<Food, Integer> {
+}
